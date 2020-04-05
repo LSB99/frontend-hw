c84d5c023a648903d78ae4848e743756ed5eea50
Lim, Subin <elf9907@gmail.com>
2020-04-05 15:18:11 +0900

    프론트엔드 3주차 과제

--- 3주차 과제/vowel.js
+++ 3주차 과제/vowel.js
@@ -1,30 +1,10 @@
 let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
 
-for(let i=0;i<s.length-1;i++){
-    
-    let t = s.slice(i,i+1);
- 
-    if(t=='a' || t=='A'){
-        console.log(i);
-    }
+let change = s.replace(/[aeiouAEIOU]/g,"모");
 
-    else if(t=='e' || t=='E'){
-        console.log(i);
-    }
-
-    else if(t=='i' || t=='I'){
-        console.log(i);
-    }
-
-    else if(t=='o' || t=='O'){
-        console.log(i);
-    }
-
-    else if(t=='u' || t=='U'){
+for(let i=0;i<change.length;i++){
+    if(change[i]=="모"){
         console.log(i);
     }
 }
-
-
-
 
