35a94b0d5debdb8e1eac03aa9da61256e15f2818
Lim, Subin <elf9907@gmail.com>
2020-04-05 15:28:35 +0900

    프론트엔드 3주차 과제

--- 3주차 과제/space.js
+++ 3주차 과제/space.js
@@ -1,4 +1,5 @@
 let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
 
-console.log(s.replace(/[" "]/g, ""));
+let change = s.replace(/[" "]/g, "")
+console.log(change);
 
