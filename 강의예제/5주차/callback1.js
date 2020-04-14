4cbc0e0924a7e410eda6490f74b371109955a85d
Lim Subin <elf9907@gmail.com>
2020-04-14 12:04:59 +0900

    5주차 강의예제

---
+++ 강의예제/5주차/callback1.js
@@ -0,0 +1,13 @@
+let obj = {
+    count : 0,
+    startTimer : function(){
+        console.log(this.count);
+        let callback = function(){
+            console.log(this.count++);
+        };
+        setInterval(callback, 1000);
+    }
+}
+
+
+obj.startTimer();
